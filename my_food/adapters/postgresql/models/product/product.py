9cdf238825957e6142c8f04217337bd96c68e2ee
Kapizany <kapizany@gmail.com>
2023-06-28 09:12:45 -0300

    feat(product): product postgresql adapter

---
+++ my_food/adapters/postgresql/models/product/product.py
@@ -0,0 +1,24 @@
+import uuid
+from sqlalchemy import Column, Integer, String, Enum, Float
+from sqlalchemy.dialects.postgresql import UUID
+from my_food.adapters.postgresql.database import Base, engine
+from my_food.adapters.postgresql.repositories.mixins.crud import CRUDMixin
+from my_food.application.domain.aggregates.product.interfaces.product_entity import (
+    ProductCategory,
+)
+
+
+class ProductModel(Base, CRUDMixin):
+    __tablename__ = "product"
+
+    name = Column(String, nullable=False)
+    category: Column(Enum(ProductCategory), index=True, nullable=False)
+    price = Column(Float, nullable=False)
+    description = Column(String, nullable=False)
+    image = Column(String, nullable=False)
+    uuid = Column(UUID(as_uuid=True), default=uuid.uuid4, index=True, unique=True)
+    id = Column(Integer, primary_key=True)
+
+
+ProductModel.metadata.bind = engine
+ProductModel.metadata.create_all(engine)
---
+++ my_food/adapters/postgresql/repositories/product/product.py
@@ -0,0 +1,98 @@
+from typing import Optional
+
+from my_food.adapters.postgresql.models.product.product import ProductModel
+from my_food.application.domain.aggregates.product.interfaces.product_entity import (
+    ProductCategory,
+    ProductInterface,
+)
+from my_food.application.domain.aggregates.product.interfaces.product_repository import (
+    ProductRepositoryDto,
+    ProductRepositoryInterface,
+)
+
+
+class ProductRepository(ProductRepositoryInterface):
+    def create(self, entity: ProductInterface) -> None:
+        new_product = ProductModel(
+            name=entity.name,
+            category=entity.category,
+            price=entity.price,
+            description=entity.description,
+            image=entity.image,
+            uuid=entity.uuid,
+        )
+        new_product.create()
+
+    def find(self, uuid: str) -> Optional[ProductRepositoryDto]:
+        product = ProductModel.retrieve(uuid)
+        if product is None:
+            return None
+        return ProductRepositoryDto(
+            name=product.name,
+            category=product.category,
+            price=product.price,
+            description=product.description,
+            image=product.image,
+            uuid=str(product.uuid),
+        )
+
+    def update(self, entity: ProductInterface) -> None:
+        product = ProductModel.retrieve(entity.uuid)
+        if product:
+            ProductModel.update(
+                {
+                    "name": entity.name,
+                    "category": entity.category,
+                    "price": entity.price,
+                    "description": entity.description,
+                    "image": entity.image,
+                    "uuid": entity.uuid,
+                    "id": product.id,
+                }
+            )
+
+    def list(self) -> Optional[ProductRepositoryDto]:
+        products = ProductModel.list()
+
+        return [
+            ProductRepositoryDto(
+                name=product.name,
+                category=product.category,
+                price=product.price,
+                description=product.description,
+                image=product.image,
+                uuid=str(product.uuid),
+            )
+            for product in products
+        ]
+
+    def delete(self, uuid: str) -> Optional[ProductRepositoryDto]:
+        product = ProductModel.retrieve(uuid)
+        if product is None:
+            return None
+        ProductModel.destroy(product.uuid)
+        return ProductRepositoryDto(
+            name=product.name,
+            category=product.category,
+            price=product.price,
+            description=product.description,
+            image=product.image,
+            uuid=str(product.uuid),
+        )
+
+    def filter_by_category(
+        self, category: ProductCategory
+    ) -> Optional[ProductRepositoryDto]:
+        products = ProductModel.list_filtering_by_column("category", category)
+
+        return [
+            ProductRepositoryDto(
+                name=product.name,
+                category=product.category,
+                price=product.price,
+                description=product.description,
+                image=product.image,
+                uuid=str(product.uuid),
+            )
+            for product in products
+        ]

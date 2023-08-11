b66ced6eb516daf569a882e60f3898b4b949bbb9
Kapizany <kapizany@gmail.com>
2023-08-11 08:54:18 -0300

    build(k8s): fix api deployment and secrets

--- init_app.sh
+++ init_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 alembic upgrade head
-uvicorn src.adapters.FastAPI.main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn src.infrastructure.FastAPI.main:app --host 0.0.0.0 --port 8000 --reload

50df33f8c96678a8d9558d70bc9f8c3b43f07cda
henrique-pimenta <94808259+henrique-pimenta@users.noreply.github.com>
2023-08-11 22:13:01 -0300

    refactor: add order controller

--- src/infrastructure/FastAPI/views/order.py
+++ src/infrastructure/FastAPI/views/order.py
@@ -2,36 +2,18 @@ from typing import Annotated, List, Optional
 from fastapi import APIRouter, HTTPException, status as status_code, Depends
 from src.infrastructure.FastAPI.utils.auth import get_current_user_optional
 from src.infrastructure.FastAPI.utils.schemas import CreateOrderSchema
-
 from src.infrastructure.postgresql.repositories.order.order import OrderRepository
 from src.infrastructure.postgresql.repositories.product.product import ProductRepository
 from src.infrastructure.postgresql.repositories.user.user import UserRepository
-from src.domain.aggregates.order.interfaces.order_entity import (
-    OrderStatus,
-)
 from src.domain.shared.exceptions.base import DomainException
+from src.interface_adapters.controllers.order import OrderController
 from src.interface_adapters.presenters.auth import EmptyUser
-from src.use_cases.order.create.create_order import CreateOrderUseCase
-from src.use_cases.order.create.create_order_dto import (
-    CreateOrderInputDto,
-    CreateOrderOutputDto,
-)
-from src.use_cases.order.delete.delete_order import DeleteOrderUseCase
-from src.use_cases.order.delete.delete_order_dto import (
-    DeleteOrderInputDto,
-    DeleteOrderOutputDto,
-)
-from src.use_cases.order.find.find_order import FindOrderUseCase
-from src.use_cases.order.find.find_order_dto import (
-    FindOrderInputDto,
-    FindOrderOutputDto,
-)
-from src.use_cases.order.list.list_order import ListOrderUseCase
+from src.use_cases.order.create.create_order_dto import CreateOrderOutputDto
+from src.use_cases.order.delete.delete_order_dto import DeleteOrderOutputDto
+from src.use_cases.order.find.find_order_dto import FindOrderOutputDto
 from src.use_cases.order.list.list_order_dto import ListOrderOutputDto
-from src.use_cases.order.update.update_order import UpdateOrderUseCase
 from src.use_cases.order.update.update_order_dto import (
     UpdateOrderInputDto,
-    UpdateOrderItemInputDto,
     UpdateOrderOutputDto,
     UpdateStatusOrderInputDto,
 )
@@ -49,17 +31,12 @@ async def create_order(
     ],
 ) -> CreateOrderOutputDto:
     try:
-        order_repository = OrderRepository()
-        product_repository = ProductRepository()
-        user_repository = UserRepository()
-        user_uuid = current_user.uuid if current_user else None
-        create_use_case = CreateOrderUseCase(
-            order_repository, product_repository, user_repository
+        return OrderController(OrderRepository()).create_order(
+            input_data,
+            ProductRepository(),
+            UserRepository(),
+            current_user,
         )
-        new_user = create_use_case.execute(
-            CreateOrderInputDto(items=input_data.items, user_uuid=user_uuid)
-        )
-        return new_user
     except DomainException as err:
         raise HTTPException(
             status_code=status_code.HTTP_400_BAD_REQUEST,
@@ -71,13 +48,7 @@ async def create_order(
 @router.get("/", status_code=200)
 async def list_orders(status: str | None = None) -> Optional[List[ListOrderOutputDto]]:
     try:
-        repository = OrderRepository()
-        list_use_case = ListOrderUseCase(repository)
-        filters = {}
-        if status is not None:
-            filters["status"] = OrderStatus(status).name
-        orders = list_use_case.execute(filters)
-        return orders
+        return OrderController(OrderRepository()).list_orders(status)
     except DomainException as err:
         raise HTTPException(
             status_code=status_code.HTTP_400_BAD_REQUEST,
@@ -87,18 +58,15 @@ async def list_orders(status: str | None = None) -> Optional[List[ListOrderOutpu
 
 
 @router.put("/", status_code=200)
-async def update_orders(
+async def update_order(
     input_data: UpdateOrderInputDto,
 ) -> Optional[UpdateOrderOutputDto]:
     try:
-        order_repository = OrderRepository()
-        product_repository = ProductRepository()
-        user_repository = UserRepository()
-        update_use_case = UpdateOrderUseCase(
-            order_repository, product_repository, user_repository
+        return OrderController(OrderRepository()).update_order(
+            input_data,
+            ProductRepository(),
+            UserRepository(),
         )
-        orders = update_use_case.execute(input_data)
-        return orders
     except DomainException as err:
         raise HTTPException(
             status_code=status_code.HTTP_400_BAD_REQUEST,
@@ -110,10 +78,7 @@ async def update_orders(
 @router.get("/{order_uuid}/", status_code=200, response_model=FindOrderOutputDto)
 async def retireve_order(order_uuid: str):
     try:
-        repository = OrderRepository()
-        find_use_case = FindOrderUseCase(repository)
-        order = find_use_case.execute(FindOrderInputDto(uuid=order_uuid))
-        return order
+        return OrderController(OrderRepository()).retireve_order(order_uuid)
     except DomainException as err:
         raise HTTPException(
             status_code=status_code.HTTP_400_BAD_REQUEST,
@@ -125,10 +90,7 @@ async def retireve_order(order_uuid: str):
 @router.delete("/{order_uuid}/", status_code=200, response_model=DeleteOrderOutputDto)
 async def delete_order(order_uuid: str):
     try:
-        repository = OrderRepository()
-        delete_use_case = DeleteOrderUseCase(repository)
-        order = delete_use_case.execute(DeleteOrderInputDto(uuid=order_uuid))
-        return order
+        return OrderController(OrderRepository()).delete_order(order_uuid)
     except DomainException as err:
         raise HTTPException(
             status_code=status_code.HTTP_400_BAD_REQUEST,
@@ -138,35 +100,17 @@ async def delete_order(order_uuid: str):
 
 
 @router.put("/{order_uuid}/update-status/", status_code=200)
-async def update_status_order(
-    input_data: UpdateStatusOrderInputDto,
+async def update_order_status(
     order_uuid: str,
-) -> Optional[UpdateOrderOutputDto]:
+    input_data: UpdateStatusOrderInputDto,
+) -> UpdateOrderOutputDto:
     try:
-        order_repository = OrderRepository()
-        product_repository = ProductRepository()
-        user_repository = UserRepository()
-
-        find_use_case = FindOrderUseCase(order_repository)
-        order = find_use_case.execute(FindOrderInputDto(uuid=order_uuid))
-        order = UpdateOrderInputDto(
-            items=[
-                UpdateOrderItemInputDto(
-                    comment=item.comment,
-                    product_uuid=item.product["uuid"],
-                    quantity=item.quantity,
-                )
-                for item in order.items
-            ],
-            status=input_data.status,
-            uuid=order.uuid,
+        return OrderController(OrderRepository()).update_order_status(
+            order_uuid,
+            input_data,
+            ProductRepository(),
+            UserRepository(),
         )
-
-        update_use_case = UpdateOrderUseCase(
-            order_repository, product_repository, user_repository
-        )
-        orders = update_use_case.execute(order)
-        return orders
     except DomainException as err:
         raise HTTPException(
             status_code=status_code.HTTP_400_BAD_REQUEST,
---
+++ src/interface_adapters/controllers/order.py
@@ -0,0 +1,117 @@
+from typing import List, Optional
+from src.domain.aggregates.order.interfaces.order_entity import OrderStatus
+from src.domain.aggregates.order.interfaces.order_repository import (
+    OrderRepositoryInterface,
+)
+from src.domain.aggregates.product.interfaces.product_repository import (
+    ProductRepositoryInterface,
+)
+from src.domain.aggregates.user.interfaces.user_repository import (
+    UserRepositoryInterface,
+)
+from src.interface_adapters.presenters.auth import EmptyUser
+from src.use_cases.order.create.create_order import CreateOrderUseCase
+from src.use_cases.order.create.create_order_dto import (
+    CreateOrderInputDto,
+    CreateOrderOutputDto,
+)
+from src.use_cases.order.delete.delete_order import DeleteOrderUseCase
+from src.use_cases.order.delete.delete_order_dto import (
+    DeleteOrderInputDto,
+    DeleteOrderOutputDto,
+)
+from src.use_cases.order.find.find_order import FindOrderUseCase
+from src.use_cases.order.find.find_order_dto import (
+    FindOrderInputDto,
+    FindOrderOutputDto,
+)
+from src.use_cases.order.list.list_order import ListOrderUseCase
+from src.use_cases.order.list.list_order_dto import ListOrderOutputDto
+from src.use_cases.order.update.update_order import UpdateOrderUseCase
+from src.use_cases.order.update.update_order_dto import (
+    UpdateOrderInputDto,
+    UpdateOrderItemInputDto,
+    UpdateOrderOutputDto,
+    UpdateStatusOrderInputDto,
+)
+from src.use_cases.user.find.find_user_dto import FindUserOutputDto
+
+
+class OrderController:
+    def __init__(self, repository: OrderRepositoryInterface):
+        self.repository = repository
+
+    def create_order(
+        self,
+        input_data,
+        product_repository: ProductRepositoryInterface,
+        user_repository: UserRepositoryInterface,
+        current_user: FindUserOutputDto | EmptyUser,
+    ) -> CreateOrderOutputDto:
+        create_use_case = CreateOrderUseCase(
+            self.repository, product_repository, user_repository
+        )
+        new_user = create_use_case.execute(
+            CreateOrderInputDto(items=input_data.items, user_uuid=current_user.uuid)
+        )
+        return new_user
+
+    def list_orders(self, status: str | None) -> Optional[List[ListOrderOutputDto]]:
+        list_use_case = ListOrderUseCase(self.repository)
+
+        filters = {}
+        if status is not None:
+            filters["status"] = OrderStatus(status).name
+
+        orders = list_use_case.execute(filters)
+        return orders
+
+    def update_order(
+        self,
+        input_data: UpdateOrderInputDto,
+        product_repository: ProductRepositoryInterface,
+        user_repository: UserRepositoryInterface,
+    ) -> UpdateOrderOutputDto:
+        update_use_case = UpdateOrderUseCase(
+            self.repository, product_repository, user_repository
+        )
+        orders = update_use_case.execute(input_data)
+        return orders
+
+    def retireve_order(self, order_uuid: str) -> FindOrderOutputDto:
+        find_use_case = FindOrderUseCase(self.repository)
+        order = find_use_case.execute(FindOrderInputDto(uuid=order_uuid))
+        return order
+
+    def delete_order(self, order_uuid: str) -> DeleteOrderOutputDto:
+        delete_use_case = DeleteOrderUseCase(self.repository)
+        order = delete_use_case.execute(DeleteOrderInputDto(uuid=order_uuid))
+        return order
+
+    def update_order_status(
+        self,
+        order_uuid: str,
+        input_data: UpdateStatusOrderInputDto,
+        product_repository: ProductRepositoryInterface,
+        user_repository: UserRepositoryInterface,
+    ) -> UpdateOrderOutputDto:
+        find_use_case = FindOrderUseCase(self.repository)
+        order = find_use_case.execute(FindOrderInputDto(uuid=order_uuid))
+        order = UpdateOrderInputDto(
+            items=[
+                UpdateOrderItemInputDto(
+                    comment=item.comment,
+                    product_uuid=item.product.uuid,
+                    quantity=item.quantity,
+                )
+                for item in order.items
+            ],
+            status=input_data.status,
+            uuid=order.uuid,
+        )
+
+        update_use_case = UpdateOrderUseCase(
+            self.repository, product_repository, user_repository
+        )
+        orders = update_use_case.execute(order)
+        return orders

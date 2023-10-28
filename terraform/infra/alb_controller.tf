3e965f5b4454923927b5296d95f51d43a21a49da
henrique-pimenta <94808259+henrique-pimenta@users.noreply.github.com>
2023-10-28 16:06:40 -0300

    build: fix aws_iam_policy_document

--- terraform/infra/alb_controller.tf
+++ terraform/infra/alb_controller.tf
@@ -6,7 +6,7 @@ data "aws_iam_policy_document" "aws_load_balancer_controller_assume_role_policy"
     condition {
       test     = "StringEquals"
       variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
-      values   = ["system:serviceaccount:kube-system:aws-load-balancer-controller"]
+      values   = ["system:serviceaccount:default:aws-load-balancer-controller-role"]
     }
 
     principals {

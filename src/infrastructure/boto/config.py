0eb29edacf3dbc374577c75a986e3f0d78e93396
Kapizany <kapizany@gmail.com>
2023-11-01 21:17:11 -0300

    fix: fix aws region var name

--- src/infrastructure/boto/config.py
+++ src/infrastructure/boto/config.py
@@ -5,5 +5,5 @@ import boto3
 session = boto3.Session(
     aws_access_key_id=config("AWS_ACCESS_KEY_ID"),
     aws_secret_access_key=config("AWS_SECRET_ACCESS_KEY"),
-    region_name=config("AWS_REGION_NAME"),
+    region_name=config("AWS_REGION"),
 )
